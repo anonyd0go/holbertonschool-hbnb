6a4fc7a7d104eddac5cc8d3b83e1067d1bf6354f
DDF <133790676+anonyd0go@users.noreply.github.com>
2025-02-26 16:09:02 -0400

    Correct pycodestyle in amenity.py

--- part2/hbnb/app/models/amenity.py
+++ part2/hbnb/app/models/amenity.py
@@ -1,14 +1,15 @@
 from basecls import BaseModel
 
+
 class Amenity(BaseModel):
     def __init__(self, name):
         super().__init__()
         self._name = name
-    
+
     @property
     def name(self):
         return self._name
-    
+
     @name.setter
     def name(self, name):
         if len(name) > 50:
