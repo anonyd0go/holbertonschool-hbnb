c9df1628dc76f5f070fd8ecb1521909fe1712870
DDF <133790676+anonyd0go@users.noreply.github.com>
2025-02-26 16:55:51 -0400

    Add docstrings to config.py

--- part2/hbnb/config.py
+++ part2/hbnb/config.py
@@ -2,11 +2,20 @@ import os
 
 
 class Config:
+    """
+    Base configuration class. Contains default configuration settings.
+    """
     SECRET_KEY = os.getenv('SECRET_KEY', 'default_secret_key')
     DEBUG = False
 
+
 class DevelopmentConfig(Config):
+    """
+    Development configuration class. Inherits from Config and sets DEBUG to 
+    True.
+    """
     DEBUG = True
+
 
 config = {
     'development': DevelopmentConfig,
