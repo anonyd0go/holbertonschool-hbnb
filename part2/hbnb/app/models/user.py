7f5c012843c05c11b5dbb545c830ae5622d89bf3
DDF <133790676+anonyd0go@users.noreply.github.com>
2025-02-26 12:37:56 -0400

    Update getters and setters for user class user.py

--- part2/hbnb/app/models/user.py
+++ part2/hbnb/app/models/user.py
@@ -3,12 +3,40 @@ from basecls import BaseModel
 class User(BaseModel):
     def __init__(self, first_name, last_name, email, is_admin=False):
         super().__init__()
-        self.first_name = first_name[:51]
-        self.last_name = last_name[:51]
-        self.email = email
-        self.is_admin = is_admin
+        self._first_name = first_name
+        self._last_name = last_name
+        self._email = email
+        self.__is_admin = is_admin
         self.places = []
 
 
     def add_place(self, place):
         self.places.append(place)
+    
+    @property
+    def first_name(self):
+        return self._first_name
+    
+    @first_name.setter
+    def first_name(self, first_name):
+        if len(first_name) > 50:
+            raise ValueError
+        self._first_name = first_name
+
+    @property
+    def last_name(self):
+        return self._last_name
+    
+    @last_name.setter
+    def last_name(self, last_name):
+        if len(last_name) > 50:
+            raise ValueError
+        self._last_name = last_name
+    
+    @property
+    def email(self):
+        return self._email
+    
+    @email.setter
+    def email(self, email):
+        self._email = email
