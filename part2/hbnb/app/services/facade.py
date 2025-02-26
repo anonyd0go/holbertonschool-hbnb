b325788d96c52fc34114761b4a79d2ead2ee8138
DDF <133790676+anonyd0go@users.noreply.github.com>
2025-02-26 17:16:14 -0400

    Add docstrings to app/services/facade.py

--- part2/hbnb/app/services/facade.py
+++ part2/hbnb/app/services/facade.py
@@ -1,7 +1,16 @@
 from app.persistence.repository import InMemoryRepository
 
+
 class HBnBFacade:
+    """
+    Facade class to manage interactions between different layers of the
+    application.
+    """
     def __init__(self):
+        """
+        Initialize the HBnBFacade with in-memory repositories for users,
+        places, reviews, and amenities.
+        """
         self.user_repo = InMemoryRepository()
         self.place_repo = InMemoryRepository()
         self.review_repo = InMemoryRepository()
@@ -9,10 +18,22 @@ class HBnBFacade:
 
     # Placeholder method for creating a user
     def create_user(self, user_data):
+        """
+        Placeholder method for creating a user.
+
+        Args:
+            user_data: A dictionary containing user data.
+        """
         # Logic will be implemented in later tasks
         pass
 
     # Placeholder method for fetching a place by ID
     def get_place(self, place_id):
+        """
+        Placeholder method for fetching a place by ID.
+
+        Args:
+            place_id: The ID of the place to fetch.
+        """
         # Logic will be implemented in later tasks
         pass
