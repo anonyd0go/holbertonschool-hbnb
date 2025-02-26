91a77d204efbdfbedcaad768477ae1c7d3c2b07c
DDF <133790676+anonyd0go@users.noreply.github.com>
2025-02-26 15:54:17 -0400

    Add getter and setter for several Review properties in review.py

--- part2/hbnb/app/models/review.py
+++ part2/hbnb/app/models/review.py
@@ -7,3 +7,41 @@ class Review(BaseModel):
         self.rating = rating
         self.place = place
         self.user = user
+
+    @property
+    def text(self):
+        return self._text
+
+    @text.setter
+    def text(self, text):
+        if type(text) is not str:
+            raise TypeError
+        self._text = text
+
+    @property
+    def rating(self):
+        return self._rating
+
+    @rating.setter
+    def rating(self, rating):
+        if type(rating) is not int:
+            raise TypeError
+        if rating not in range(1, 6):
+            raise ValueError
+        self._rating = rating
+
+    @property
+    def place(self):
+        return self._place
+    
+    @place.setter
+    def place(self, place):
+        self._place = place
+
+    @property
+    def user(self):
+        return self._user
+    
+    @user.setter
+    def user(self, user):
+        self._user = user
