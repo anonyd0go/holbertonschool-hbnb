655d1f72c95c736f49935ff4a0ecbce9a6b0c31b
DDF <133790676+anonyd0go@users.noreply.github.com>
2025-02-28 11:46:04 -0400

    Add amenity CURD operations to facade in facade.py

--- part2/hbnb/app/services/facade.py
+++ part2/hbnb/app/services/facade.py
@@ -1,5 +1,6 @@
 from app.persistence.repository import InMemoryRepository
 from app.models.user import User
+from app.models.amenity import Amenity
 
 class HBnBFacade:
     """
@@ -15,6 +16,8 @@ class HBnBFacade:
         self.place_repo = InMemoryRepository()
         self.review_repo = InMemoryRepository()
         self.amenity_repo = InMemoryRepository()
+
+#--------------User facade CURD ops--------------#
 
     def create_user(self, user_data):
         """
@@ -63,7 +66,7 @@ class HBnBFacade:
         """
         return self.user_repo.get_all()
     
-    def update_user(self, user_id, data):
+    def update_user(self, user_id, user_data):
         """
         Update a user's information.
 
@@ -71,9 +74,25 @@ class HBnBFacade:
             user_id (str): The ID of the user to update.
             data (dict): A dictionary of updated user data.
         """
-        self.user_repo.update(user_id, data)
+        self.user_repo.update(user_id, user_data)
 
-    # Placeholder method for fetching a place by ID
+#--------------Amenity facade CURD ops--------------#
+
+    def create_amenity(self, amenity_data):
+        amenity = Amenity(**amenity_data)
+        self.amenity_repo.add(amenity)
+        return amenity
+
+    def get_amenity(self, amenity_id):
+        return self.amenity_repo.get(amenity_id)
+
+    def get_all_amenities(self):
+        return self.amenity_repo.get_all()
+
+    def update_amenity(self, amenity_id, amenity_data):
+        self.amenity_repo.update(amenity_id, amenity_data)
+
+#--------------Place facade CURD ops--------------#
     def get_place(self, place_id):
         """
         Placeholder method for fetching a place by ID.
