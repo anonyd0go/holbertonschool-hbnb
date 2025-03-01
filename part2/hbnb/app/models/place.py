8bbc7bc0c7308f9c2f153adf8474f12ab25b8641
DDF <133790676+anonyd0go@users.noreply.github.com>
2025-03-01 18:06:16 -0400

    Add amenities and reviews getters and setters for Place class in place.py

--- part2/hbnb/app/models/place.py
+++ part2/hbnb/app/models/place.py
@@ -7,7 +7,7 @@ class Place(BaseModel):
     Represents a place with various attributes and related
     reviews and amenities.
     """
-    def __init__(self, title, description, price, latitude, longitude, owner):
+    def __init__(self, title, description, price, latitude, longitude, owner_id):
         """
         Initialize a Place instance.
 
@@ -25,9 +25,9 @@ class Place(BaseModel):
         self.price = price
         self.latitude = latitude
         self.longitude = longitude
-        self.owner = owner
-        self.reviews = []  # List to store related reviews
-        self.amenities = []  # List to store related amenities
+        self.owner_id = owner_id
+        self._reviews = []  # List to store related reviews
+        self._amenities = []  # List to store related amenities
 
     def add_review(self, review):
         """
@@ -36,7 +36,38 @@ class Place(BaseModel):
         Args:
             review: The review to add.
         """
-        self.reviews.append(review)
+        self._reviews.append(review)
+
+    @property
+    def reviews(self):
+        """
+        Get the list of reviews associated with the place.
+
+        Returns:
+            list: The list of reviews.
+        """
+        return self._reviews
+
+    @reviews.setter
+    def reviews(self, reviews):
+        """
+        Set the list of reviews associated with the place.
+
+        Args:
+            reviews (str or list): A review or a list of reviews to add.
+
+        Raises:
+            TypeError: If the reviews is not a string or a list of strings.
+        """
+        if type(reviews) is str:
+            self.add_review(reviews)
+            self._reviews = self.reviews
+        elif type(reviews) is list:
+            if not all(isinstance(rev, str) for rev in reviews):
+                raise TypeError("Place reviews must be a list of str")
+            self._reviews = self.reviews + reviews
+        else:
+            raise TypeError("Place reviews can only be list or str")
 
     def add_amenity(self, amenity):
         """
@@ -45,7 +76,38 @@ class Place(BaseModel):
         Args:
             amenity: The amenity to add.
         """
-        self.amenities.append(amenity)
+        self._amenities.append(amenity)
+
+    @property
+    def amenities(self):
+        """
+        Get the list of amenities associated with the place.
+
+        Returns:
+            list: The list of amenities.
+        """
+        return self._amenities
+
+    @amenities.setter
+    def amenities(self, amenities):
+        """
+        Set the list of amenities associated with the place.
+
+        Args:
+            amenities (str or list): An amenity or a list of amenities to add.
+
+        Raises:
+            TypeError: If the amenities is not a string or a list of strings.
+        """
+        if type(amenities) is str:
+            self.add_amenity(amenities)
+            self._amenities = self.amenities
+        elif type(amenities) is list:
+            if not all(isinstance(ame, str) for ame in amenities):
+                raise TypeError("Place amenities must be a list of str")
+            self._amenities = self.amenities + amenities
+        else:
+            raise TypeError("Place amenities can only be str or list")
 
     @property
     def title(self):
@@ -66,8 +128,11 @@ class Place(BaseModel):
             title (str): The new title of the place.
 
         Raises:
+            TypeError: If the title is not a string.
             ValueError: If the title is longer than 100 characters.
         """
+        if type(title) is not str:
+            raise TypeError("Place title must be a str")
         if len(title) > 100:
             raise ValueError("Place title is longer than 100 chars")
         self._title = title
@@ -89,7 +154,12 @@ class Place(BaseModel):
 
         Args:
             description (str): The new description of the place.
+
+        Raises:
+            TypeError: If the description is not a string.
         """
+        if type(description) is not str:
+            raise TypeError("Place description must be a str")
         self._description = description
 
     @property
@@ -177,21 +247,26 @@ class Place(BaseModel):
         self._longitude = longitude
 
     @property
-    def owner(self):
+    def owner_id(self):
         """
         Get the owner of the place.
 
         Returns:
             str: The owner of the place.
         """
-        return self._owner
+        return self._owner_id
 
-    @owner.setter
-    def owner(self, owner):
+    @owner_id.setter
+    def owner_id(self, owner_id):
         """
         Set the owner of the place.
 
         Args:
             owner (str): The new owner of the place.
+
+        Raises:
+            TypeError: If the owner_id is not a str.
         """
-        self._owner = owner
+        if not isinstance(owner_id, str):
+            raise TypeError("Place owner_id must be a str")
+        self._owner_id = owner_id
