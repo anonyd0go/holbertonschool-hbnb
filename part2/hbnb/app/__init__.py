ee64103f60da8a4dcf10f274227abf59bb47bd4c
DDF <133790676+anonyd0go@users.noreply.github.com>
2025-03-02 12:28:48 -0400

    Update app/__init__.py to include all uris for the api

--- part2/hbnb/app/__init__.py
+++ part2/hbnb/app/__init__.py
@@ -2,6 +2,8 @@ from flask import Flask
 from flask_restx import Api
 from app.api.v1.users import api as users_ns
 from app.api.v1.amenities import api as amenities_ns
+from app.api.v1.places import api as places_ns
+from app.api.v1.reviews import api as reviews_ns
 
 
 def create_app():
@@ -20,8 +22,10 @@ def create_app():
         doc='/api/vi/'
     )
 
-    # Additional namespaces for places and reviews will be added
+    # API namespaces for respective URIs
     api.add_namespace(users_ns, path='/api/v1/users')
     api.add_namespace(amenities_ns, path='/api/v1/amenities')
+    api.add_namespace(places_ns, path='/api/v1/places')
+    api.add_namespace(reviews_ns, path='/api/v1/reviews')
 
     return app
