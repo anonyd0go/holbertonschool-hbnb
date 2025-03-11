62b485e5fd626c994d9588b02b9043b58bbaee84
DDF <133790676+anonyd0go@users.noreply.github.com>
2025-03-11 14:47:53 -0400

    Update app/__init__.py file

--- part3/hbnb/app/__init__.py
+++ part3/hbnb/app/__init__.py
@@ -1,10 +1,12 @@
 from flask import Flask
-from app.extensions import bcrypt
+from app.extensions import bcrypt, jwt
 from flask_restx import Api
 from app.api.v1.users import api as users_ns
 from app.api.v1.amenities import api as amenities_ns
 from app.api.v1.places import api as places_ns
 from app.api.v1.reviews import api as reviews_ns
+from app.api.v1.auth import api as auth_ns
+from app.api.v1.protected import api as protected_ns
 
 
 def create_app(config_class="config.DevelopmentConfig"):
@@ -17,6 +19,7 @@ def create_app(config_class="config.DevelopmentConfig"):
     app = Flask(__name__)
     app.config.from_object(config_class)
     bcrypt.init_app(app)
+    jwt.init_app(app)
     api = Api(
         app,
         version='1.0',
@@ -30,5 +33,7 @@ def create_app(config_class="config.DevelopmentConfig"):
     api.add_namespace(amenities_ns, path='/api/v1/amenities')
     api.add_namespace(places_ns, path='/api/v1/places')
     api.add_namespace(reviews_ns, path='/api/v1/reviews')
+    api.add_namespace(auth_ns, path='/api/v1/auth')
+    api.add_namespace(protected_ns, path='/api/v1/protected')
 
     return app
