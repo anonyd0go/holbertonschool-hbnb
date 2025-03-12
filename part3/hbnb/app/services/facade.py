4bfc19754d4dfa95fede741e7559a9caca77eb5d
DDF <133790676+anonyd0go@users.noreply.github.com>
2025-03-12 13:16:52 -0400

    Change repositories patter from inmemory to db sqlalchemy in facade.py

--- part3/hbnb/app/services/facade.py
+++ part3/hbnb/app/services/facade.py
@@ -1,4 +1,4 @@
-from app.persistence.repository import InMemoryRepository
+from app.persistence.repository import InMemoryRepository, SQLAlchemyRepository
 from app.models.user import User
 from app.models.amenity import Amenity
 from app.models.place import Place
@@ -15,10 +15,10 @@ class HBnBFacade:
         Initialize the HBnBFacade with in-memory repositories for users,
         places, reviews, and amenities.
         """
-        self.user_repo = InMemoryRepository()
-        self.place_repo = InMemoryRepository()
-        self.review_repo = InMemoryRepository()
-        self.amenity_repo = InMemoryRepository()
+        self.user_repo = SQLAlchemyRepository(User)
+        self.place_repo = SQLAlchemyRepository(Place)
+        self.review_repo = SQLAlchemyRepository(Review)
+        self.amenity_repo = SQLAlchemyRepository(Amenity)
 
 #--------------User facade CRUD ops--------------#
     def create_user(self, user_data):
