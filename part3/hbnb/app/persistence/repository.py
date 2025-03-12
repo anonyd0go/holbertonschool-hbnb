90106196b05ef22e080fe236fa04e08b20a58d6a
DDF <133790676+anonyd0go@users.noreply.github.com>
2025-03-12 13:33:39 -0400

    Add docstrings to sqlalchemyrepo class in persistence/repository.py

--- part3/hbnb/app/persistence/repository.py
+++ part3/hbnb/app/persistence/repository.py
@@ -77,20 +77,58 @@ class Repository(ABC):
 
 
 class SQLAlchemyRepository(Repository):
+    """
+    SQLAlchemy implementation of the Repository interface.
+    """
+
     def __init__(self, model):
+        """
+        Initialize the SQLAlchemy repository.
+
+        Args:
+            model: The SQLAlchemy model class.
+        """
         self.model = model
 
     def add(self, obj):
+        """
+        Add an object to the SQLAlchemy repository.
+
+        Args:
+            obj: The object to add.
+        """
         db.session.add(obj)
         db.session.commit()
 
     def get(self, obj_id):
+        """
+        Retrieve an object from the SQLAlchemy repository by its ID.
+
+        Args:
+            obj_id: The ID of the object to retrieve.
+
+        Returns:
+            The retrieved object, or None if not found.
+        """
         return self.model.query.get(obj_id)
 
     def get_all(self):
+        """
+        Retrieve all objects from the SQLAlchemy repository.
+
+        Returns:
+            A list of all objects in the repository.
+        """
         return self.model.query.all()
 
     def update(self, obj_id, data):
+        """
+        Update an object in the SQLAlchemy repository.
+
+        Args:
+            obj_id: The ID of the object to update.
+            data: A dictionary of updated data.
+        """
         obj = self.get(obj_id)
         if obj:
             for key, value in data.items():
@@ -98,12 +136,28 @@ class SQLAlchemyRepository(Repository):
             db.session.commit()
 
     def delete(self, obj_id):
+        """
+        Delete an object from the SQLAlchemy repository by its ID.
+
+        Args:
+            obj_id: The ID of the object to delete.
+        """
         obj = self.get(obj_id)
         if obj:
             db.session.delete(obj)
             db.session.commit()
 
     def get_by_attribute(self, attr_name, attr_value):
+        """
+        Retrieve an object from the SQLAlchemy repository by an attribute value.
+
+        Args:
+            attr_name: The name of the attribute.
+            attr_value: The value of the attribute.
+
+        Returns:
+            The retrieved object, or None if not found.
+        """
         return self.model.query.filter(getattr(self.model, attr_name) == attr_value).first()
 
 
