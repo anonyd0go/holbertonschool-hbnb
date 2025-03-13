fad1e0eae676be457f6f3403b7e1181befc8bcb5
DDF <133790676+anonyd0go@users.noreply.github.com>
2025-03-13 12:25:17 -0400

    Update BaseModel in basecls.py to have ORM best practices

--- part3/hbnb/app/models/basecls.py
+++ part3/hbnb/app/models/basecls.py
@@ -5,8 +5,7 @@ from datetime import datetime
 
 class BaseModel(db.Model):
     """
-    Base model class that provides common attributes and methods for other
-    models.
+    Base model class that provides common attributes and methods for other models.
     """
     __abstract__ = True
 
@@ -14,20 +13,20 @@ class BaseModel(db.Model):
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
     updated_at = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
 
-
-    def __init__(self):
+    def __init__(self, **kwargs):
         """
-        Initialize a new BaseModel instance with a unique ID and timestamps.
+        Initialize a new BaseModel instance with keyword arguments.
+        This passes any provided attributes (such as first_name, email, etc.)
+        to the instance, thereby triggering any associated validations.
         """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        for key, value in kwargs.items():
+            setattr(self, key, value)
 
     def save(self):
         """
         Update the updated_at timestamp whenever the object is modified.
         """
-        self.updated_at = datetime.now()
+        self.updated_at = datetime.utcnow()
 
     def update(self, data):
         """
