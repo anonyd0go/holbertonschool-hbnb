1eb37c1090184469696344f15d493242bc053712
DDF <133790676+anonyd0go@users.noreply.github.com>
2025-03-13 09:07:23 -0400

    Update basecls.py for sqlalchemy functionality

--- part3/hbnb/app/models/basecls.py
+++ part3/hbnb/app/models/basecls.py
@@ -1,12 +1,20 @@
+from app.extensions import db
 import uuid
 from datetime import datetime
 
 
-class BaseModel:
+class BaseModel(db.Model):
     """
     Base model class that provides common attributes and methods for other
     models.
     """
+    __abstract__ = True
+
+    id = db.Column(db.String(36), primary_key=True, default=lambda: str(uuid.uuid4()))
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    updated_at = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+
+
     def __init__(self):
         """
         Initialize a new BaseModel instance with a unique ID and timestamps.
