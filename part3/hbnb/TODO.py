5ba2d9bf5b4c9e6b8cee850fdc6ef00336815780
DDF <133790676+anonyd0go@users.noreply.github.com>
2025-03-13 17:23:48 -0400

    Add TODO.py file to keep track of what is left on part3 of project

---
+++ part3/hbnb/TODO.py
@@ -0,0 +1,8 @@
+# TODO add admin functionalities
+# Update API endpoints to reflect added ORM functionalities and facade updates
+# Go over authorization section of the project
+# Test database connections with curl commands
+# This is for Users, Places, Reviews, and Amenities
+# Afterwards remove the cli file and cli init from app/__init__.py
+# create mermaid.js design for database visualization
+
