2bb4e5157c06a303a6b12996765d18bda2608a46
DDF <133790676+anonyd0go@users.noreply.github.com>
2025-03-14 17:25:46 -0400

    Update TODO.py

--- part3/hbnb/TODO.py
+++ part3/hbnb/TODO.py
@@ -1,6 +1,4 @@
 # TODO add admin functionalities
-# Update API endpoints to reflect added ORM functionalities and facade updates
-# Go over authorization section of the project
 # Test database connections with curl commands
 # This is for Users, Places, Reviews, and Amenities
 # Afterwards remove the cli file and cli init from app/__init__.py
